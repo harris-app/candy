ede277c01711d962d2e0399adf6db7202e4d7c53
HarrisZhang <harriszhang@live.cn>
2021-07-18 15:03:56 +0800

    u

--- app/src/main/java/com/panosen/candy/fragment/First.java
+++ app/src/main/java/com/panosen/candy/fragment/First.java
@@ -2,20 +2,23 @@ package com.panosen.candy.fragment;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
 
 import com.panosen.candy.R;
+
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class First extends Fragment {
 
+    private Button button;
+    private WebView webView;
 
     public First() {
         // Required empty public constructor
@@ -26,7 +29,22 @@ public class First extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        button = view.findViewById(R.id.button);
+        webView = view.findViewById(R.id.webView);
+
+        button.setOnClickListener(onClickListener);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        return view;
     }
 
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            webView.loadUrl("https://www.baidu.com");
+        }
+    };
 }
